ef9d3104fa00104ccccb2e7d70eafa28f6e70d85
xiaozhu260600866 <260600866@qq.com>
2020-11-26 18:21:40 +0800

    xiaozhu

--- rencaiwang_admin/src/views/setting/system.vue
+++ rencaiwang_admin/src/views/setting/system.vue
@@ -6,16 +6,37 @@
           <tyt-page-header content="网站配置开始" />
           <div class="dx-main">
             <div class="list-box">
-              <el-form ref="ruleForm" :model="ruleForm" label-width="150px" class="demo-ruleForm pt20" style="margin: 0;width:100%;min-width:600px;">
+              <el-form
+                ref="ruleForm"
+                :model="ruleForm"
+                label-width="150px"
+                class="demo-ruleForm pt20"
+                style="margin: 0;width:100%;min-width:600px;"
+              >
                 <el-tabs v-model="activeName2">
                   <el-tab-pane label="导航栏" name="first">
-                    <category :lists="data.category" action-prefix="admin/system" action-url="/admin/system" url="1" type="1" :canupload="false" :has-son="true" />
+                    <category
+                      :lists="data.category"
+                      action-prefix="admin/system"
+                      action-url="/admin/system"
+                      url="1"
+                      type="1"
+                      :canupload="false"
+                      :has-son="true"
+                    />
                   </el-tab-pane>
                   <el-tab-pane label="单位" name="sixth">
-                    <category :lists="data.category" action-prefix="admin/system" action-url="/admin/system" type="0" :canupload="false" :has-son="false" />
+                    <category
+                      :lists="data.category"
+                      action-prefix="admin/system"
+                      action-url="/admin/system"
+                      type="0"
+                      :canupload="false"
+                      :has-son="false"
+                    />
                   </el-tab-pane>
                   <el-tab-pane label="测试" name="third">
-                      <newClass></newClass>
+                    <newClass />
                   </el-tab-pane>
                 </el-tabs>
                 </el-tabs>
@@ -34,95 +55,95 @@
 
 </template>
 <style type="text/css">
-	.CinputWidth {
-		width: 500px;
-	}
+    .CinputWidth {
+        width: 500px;
+    }
 
-	.CinputWidth .el-input__inner {
-		border-color: #dcdfe6 !important;
-	}
+    .CinputWidth .el-input__inner {
+        border-color: #dcdfe6 !important;
+    }
 
-	.OinputWidth {
-		width: 100px;
-	}
+    .OinputWidth {
+        width: 100px;
+    }
 
-	.OinputWidth .el-input__inner {
-		border-color: #dcdfe6 !important;
-	}
+    .OinputWidth .el-input__inner {
+        border-color: #dcdfe6 !important;
+    }
 </style>
 <script>
-	import chooseLocation from 'xiaozhu/vue/components/admin/chooseLocation'
-  import category from 'xiaozhu/elementAdmin/components/category.vue'
-  import newClass from 'xiaozhu/elementAdmin/components/2020new_class';
-	export default {
-		components: {
-			chooseLocation,
-      category,
-      newClass
+    import chooseLocation from 'xiaozhu/vue/components/admin/chooseLocation'
+    import category from 'xiaozhu/elementAdmin/components/category.vue'
+    import newClass from 'xiaozhu/elementAdmin/components/new_class/2020new_class'
+    export default {
+        components: {
+            chooseLocation,
+            category,
+            newClass
 
-		},
-		data() {
-			return {
-				ruleForm: {
-					order_deadline: 0,
-					order_autook: 0,
-					is_make_menu: false,
-					is_siginin: false
-				},
-				formAction: '/admin/system/config',
-				activeName2: 'third',
-				dialogVisible: false,
-				getSiteName: this.getSiteName(),
-				data: this.formatData(this)
-			}
-		},
-		mounted: function() {
-			this.ajax()
-		},
-		methods: {
-			chooseLocation() {
-				this.$refs.chooseLocation.ajax()
-			},
-			locationCallBack(res) {
-				if (res.latlng) {
-					this.$refs.chooseLocation.handleClose()
-					this.ruleForm.address = res.poiaddress
-					this.ruleForm.location_x = res.latlng.lng
-					this.ruleForm.location_y = res.latlng.lat
-				}
-			},
-			submitForm(formName) {
-				this.$refs[formName].validate((valid) => {
-					if (valid) {
-						if (this.ruleForm.end_date && typeof this.ruleForm.end_date === 'object') {
-							this.ruleForm.end_date = this.dateToString(this.ruleForm.end_date, 'dateTime')
-						}
-						this.postAjax(this.formAction, this.ruleForm, function(msg) {})
-					} else {
-						console.log('error submit!!')
-						return false
-					}
-				})
-			},
-			toBing() {
-				this.dialogVisible = true
-				this.$refs.qrcode.getQrcode()
-			},
-			resetForm(formName) {
-				this.$refs[formName].resetFields()
-			},
-			ajax: function() {
-				this.getAjax(this, {}, msg => {
-					if ('detail' in msg) {
-						this.ruleForm = msg.detail
-						this.ruleForm.web_logo = this.ruleForm.web_logo ? this.splitCover(this.ruleForm.web_logo,
-							'product') : []
-						this.ruleForm.wechat_logo = this.ruleForm.wechat_logo ? this.splitCover(this.ruleForm
-							.wechat_logo, 'product') : []
-						this.ruleForm.card_user_id_default = parseInt(this.ruleForm.card_user_id_default)
-					}
-				})
-			}
-		}
-	}
+        },
+        data() {
+            return {
+                ruleForm: {
+                    order_deadline: 0,
+                    order_autook: 0,
+                    is_make_menu: false,
+                    is_siginin: false
+                },
+                formAction: '/admin/system/config',
+                activeName2: 'third',
+                dialogVisible: false,
+                getSiteName: this.getSiteName(),
+                data: this.formatData(this)
+            }
+        },
+        mounted: function() {
+            this.ajax()
+        },
+        methods: {
+            chooseLocation() {
+                this.$refs.chooseLocation.ajax()
+            },
+            locationCallBack(res) {
+                if (res.latlng) {
+                    this.$refs.chooseLocation.handleClose()
+                    this.ruleForm.address = res.poiaddress
+                    this.ruleForm.location_x = res.latlng.lng
+                    this.ruleForm.location_y = res.latlng.lat
+                }
+            },
+            submitForm(formName) {
+                this.$refs[formName].validate((valid) => {
+                    if (valid) {
+                        if (this.ruleForm.end_date && typeof this.ruleForm.end_date === 'object') {
+                            this.ruleForm.end_date = this.dateToString(this.ruleForm.end_date, 'dateTime')
+                        }
+                        this.postAjax(this.formAction, this.ruleForm, function(msg) {})
+                    } else {
+                        console.log('error submit!!')
+                        return false
+                    }
+                })
+            },
+            toBing() {
+                this.dialogVisible = true
+                this.$refs.qrcode.getQrcode()
+            },
+            resetForm(formName) {
+                this.$refs[formName].resetFields()
+            },
+            ajax: function() {
+                this.getAjax(this, {}, msg => {
+                    if ('detail' in msg) {
+                        this.ruleForm = msg.detail
+                        this.ruleForm.web_logo = this.ruleForm.web_logo ? this.splitCover(this.ruleForm.web_logo,
+                            'product') : []
+                        this.ruleForm.wechat_logo = this.ruleForm.wechat_logo ? this.splitCover(this.ruleForm
+                            .wechat_logo, 'product') : []
+                        this.ruleForm.card_user_id_default = parseInt(this.ruleForm.card_user_id_default)
+                    }
+                })
+            }
+        }
+    }
 </script>
